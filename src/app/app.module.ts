5a04199d08ab146e6aa042b74a29af351b03fa3c
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-11-15 11:31:22 -0300

    feat: modifica tela inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusServerComponent } from './component/status-server/status-server.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { PaymentsComponent } from './component/payments/payments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusServerComponent,
+    PaymentsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
